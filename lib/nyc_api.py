f43cf60e895a9c6bae96b798bd2899ea21b56a44
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-02 19:47:43 -0400

    nycapi request url

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,17 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+        programs_list.append(program["agency"])
 
+    return programs_list
 
-programs = GetPrograms().get_programs()
+#programs = GetPrograms().get_programs()
+programs = GetPrograms().program_school()
 print(programs)
+for school in set(programs):
+    print(school)
